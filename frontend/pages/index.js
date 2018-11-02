3970c8e4f7dbf524ca83ab7c0253022b8b7dc72d
Hoang Le <hoangle@Hoangs-MacBook-Pro.local>
2018-11-02 17:18:46 +0700

    Init code

---
+++ frontend/pages/index.js
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+
+const Home = props => (
+  <div>
+    <h1>Home</h1>
+    <Link href='/sell'>SELL</Link>
+  </div>
+)
+
+export default Home;
---
+++ frontend/pages/sell.js
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+
+const Sell = props => (
+  <div>
+    <h1>SELL</h1>
+    <Link href='/'>HOME</Link>
+  </div>
+)
+
+export default Sell;
